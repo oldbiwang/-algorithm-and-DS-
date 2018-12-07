43c9c35a4f6be6562a51782141ff4792e92e540f
oldbiwang <1063049578wrp@gmail.com>
2018-12-07 23:24:50 +0800

    提交代码

---
+++ Java/algorithms4/sort/Selection.java
@@ -0,0 +1,48 @@
+public class Selection {
+    public static void main(String[] args) {
+
+    }
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for(int i = 0; i < N; i++) {
+            int min = i;
+            for(int j = i + 1; j < N; j++) {
+                if(less(a[j],a[min]))
+                    min = j;
+                // if(a[j].compareTo(a[min]) == -1)
+                //     min = j;
+            }
+            exch(a, i, min);
+            // Comparable temp = a[i];
+            // a[i] = a[min];
+            // a[min] = temp;
+        }
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static void show(Comparable[] a) {
+        // 在单行中打印数组
+        for (int i = 0; i < a.length; i++) {
+            StdOut.print(a[i] + " ");
+        }
+        StdOut.println();
+    }
+
+    public static boolean isSorted(Comparable[] a) {
+        // 测试数组元素是否有序
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i - 1]))
+                return false;
+        return true;
+    }
+}
